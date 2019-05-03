e093a6a16d6a7eae21deb4856a8a2f7a15bb8740
noahmarble <mablenoa@urbandaleschools.com>
2019-05-03 12:12:14 -0500

    8.2 done

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -8,6 +8,13 @@ your program 40 times. Use a FOR loop from -20 to +20 to generate all the possib
 
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
+SecretMessage= "Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
-
-
+for i in range (-20,21):
+    encrypted_text = ""
+    for c in SecretMessage:
+        x = ord(c)
+        x = x + i
+        c2 = chr(x)
+        encrypted_text = encrypted_text + c2
+    print(encrypted_text)
